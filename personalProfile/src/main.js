be6a51bc1ce7835540de69bbd079a6b5cfe2c3df
mayi <799278834@qq.com>
2018-05-11 18:22:41 +0800

    particles

--- personalProfile/src/main.js
+++ personalProfile/src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import particles from "particles.js"
 
-Vue.config.productionTip = false
+Vue.use(particles);
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app');
--- personalProfile/src/router/index.js
+++ personalProfile/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '@/components/login'
 
 Vue.use(Router)
 
@@ -9,7 +8,10 @@ export default new Router({
     {
       path: '/',
       name: 'login',
-      component: login
+      component: resolve => require(['@/views/home/home.vue'],resolve),
+      children:[
+
+      ]
     }
   ]
 })
